d91a9cd9c6e945b5b27fc221945263f7bb4fe19f
ozgur.demirel <demirelozgur@gmail.com>
2020-03-09 10:51:27 +0100

    react playground

---
+++ src/components/App.js
@@ -0,0 +1,19 @@
+import React from "react";
+import AboutPage from "./AboutPage";
+import HomePage from "./HomePage";
+import Header from "./common/Header";
+import CoursesPage from "./CoursesPage";
+import { Route } from "react-router-dom";
+function App() {
+
+  return (
+    <div className="container-fluid">
+      <Header />
+      <Route path="/" exact component={HomePage} />
+      <Route path="/courses" component={CoursesPage} />
+      <Route path="/about" component={AboutPage} />
+    </div>
+  );
+}
+
+export default App;
